90ad3e1aec0678381f77748e03263f7a31f9347d
ankush0710 <ankushkurvey053@gmail.com>
2026-02-15 19:39:48 +0530

    Boiler Plate code written

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { Provider } from "react-redux";
+import reducer from "./reducer/reducer.js";
+import { createStore } from "redux";
+
+const store = createStore(reducer);
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>,
+);
---
+++ src/reducer/reducer.js
@@ -0,0 +1,11 @@
+
+const initState = {
+    players: []
+}
+
+const reducer = (state = initState, action) => {
+
+    return state;
+}
+
+export default reducer
