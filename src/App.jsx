0ff342637385538fb8f2b4c8788a47176b8d975a
ankush0710 <ankushkurvey053@gmail.com>
2026-02-18 19:22:47 +0530

    player's cards has been created

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import {faInstagram, faFacebook, faTwitter, faLinkedin} from '@fortawesome/free-brands-svg-icons';
-import {faPlus} from '@fortawesome/free-solid-svg-icons';
+import {faPlus, faPenToSquare, faTrash} from '@fortawesome/free-solid-svg-icons';
 import Navbar from './component/Navbar';
 import Footer from './component/Footer';
 import Players from './component/pages/Players';
 import InputPage from './component/pages/InputPage';
 
 function App() {
-  library.add(faInstagram, faFacebook, faTwitter, faLinkedin, faPlus);
+  library.add(faInstagram, faFacebook, faTwitter, faLinkedin, faPlus, faPenToSquare, faTrash);
   return (
     <>
       <BrowserRouter>
--- src/component/pages/Players.jsx
+++ src/component/pages/Players.jsx
@@ -1,10 +1,66 @@
 import React from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Players = () => {
     return(
         <>
-        <div>
-            <h1>This is Player's detail page</h1>
+        <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-4'>
+            <div id='card1' className='h-68 w-72 mx-auto bg-[#EDDCC6] mx-10 my-12 rounded-xl shadow-lg md:max-h-72 md:max-w-2xl md:'>
+                <div id="heading-section" className='py-3 border-b-4 border-double border-[#262626]'>
+                    <h1 className='text-2xl font-bold text-[#262626] text-center'>Player:-1</h1>
+                </div>
+                <div id="body-section" className='px-3 py-6 flex flex-col gap-4'>
+                    <p className='text-lg text-[#262626] font-semibold'>Player's Name: Ankush Kurvey</p>
+                    <p className='text-lg text-[#262626] font-semibold'>Score: 20 runs</p>
+                    <p className='text-lg text-[#262626] font-semibold'>Match No.: 122</p>
+                </div>
+                <div id='footer-section' className='py-2 px-5 flex justify-between items-center gap-2 border-t-4 border-double border-[#262626]'>
+                    <a href="" className='text-xl text-[#7AAACE]'><FontAwesomeIcon icon="fa-solid fa-pen-to-square"/></a>
+                    <a href="" className='text-xl text-[#7AAACE]'><FontAwesomeIcon icon="fa-solid fa-trash" /></a>
+                </div>
+            </div>
+            <div id='card2' className='h-68 w-72 mx-auto bg-[#EDDCC6] mx-10 my-12 rounded-xl shadow-lg md:max-h-72 md:max-w-2xl md:'>
+                <div id="heading-section" className='py-3 border-b-4 border-double border-[#262626]'>
+                    <h1 className='text-2xl font-bold text-[#262626] text-center'>Player:-1</h1>
+                </div>
+                <div id="body-section" className='px-3 py-6 flex flex-col gap-4'>
+                    <p className='text-lg text-[#262626] font-semibold'>Player's Name: Ankush Kurvey</p>
+                    <p className='text-lg text-[#262626] font-semibold'>Score: 20 runs</p>
+                    <p className='text-lg text-[#262626] font-semibold'>Match No.: 122</p>
+                </div>
+                <div id='footer-section' className='py-2 px-5 flex justify-between items-center gap-2 border-t-4 border-double border-[#262626]'>
+                    <a href="" className='text-xl text-[#7AAACE]'><FontAwesomeIcon icon="fa-solid fa-pen-to-square"/></a>
+                    <a href="" className='text-xl text-[#7AAACE]'><FontAwesomeIcon icon="fa-solid fa-trash" /></a>
+                </div>
+            </div>
+            <div id='card3' className='h-68 w-72 mx-auto bg-[#EDDCC6] mx-10 my-12 rounded-xl shadow-lg md:max-h-72 md:max-w-2xl md:'>
+                <div id="heading-section" className='py-3 border-b-4 border-double border-[#262626]'>
+                    <h1 className='text-2xl font-bold text-[#262626] text-center'>Player:-1</h1>
+                </div>
+                <div id="body-section" className='px-3 py-6 flex flex-col gap-4'>
+                    <p className='text-lg text-[#262626] font-semibold'>Player's Name: Ankush Kurvey</p>
+                    <p className='text-lg text-[#262626] font-semibold'>Score: 20 runs</p>
+                    <p className='text-lg text-[#262626] font-semibold'>Match No.: 122</p>
+                </div>
+                <div id='footer-section' className='py-2 px-5 flex justify-between items-center gap-2 border-t-4 border-double border-[#262626]'>
+                    <a href="" className='text-xl text-[#7AAACE]'><FontAwesomeIcon icon="fa-solid fa-pen-to-square"/></a>
+                    <a href="" className='text-xl text-[#7AAACE]'><FontAwesomeIcon icon="fa-solid fa-trash" /></a>
+                </div>
+            </div>
+            <div id='card4' className='h-68 w-72 mx-auto bg-[#EDDCC6] mx-10 my-12 rounded-xl shadow-lg md:max-h-72 md:max-w-2xl md:'>
+                <div id="heading-section" className='py-3 border-b-4 border-double border-[#262626]'>
+                    <h1 className='text-2xl font-bold text-[#262626] text-center'>Player:-1</h1>
+                </div>
+                <div id="body-section" className='px-3 py-6 flex flex-col gap-4'>
+                    <p className='text-lg text-[#262626] font-semibold'>Player's Name: Ankush Kurvey</p>
+                    <p className='text-lg text-[#262626] font-semibold'>Score: 20 runs</p>
+                    <p className='text-lg text-[#262626] font-semibold'>Match No.: 122</p>
+                </div>
+                <div id='footer-section' className='py-2 px-5 flex justify-between items-center gap-2 border-t-4 border-double border-[#262626]'>
+                    <a href="" className='text-xl text-[#7AAACE]'><FontAwesomeIcon icon="fa-solid fa-pen-to-square"/></a>
+                    <a href="" className='text-xl text-[#7AAACE]'><FontAwesomeIcon icon="fa-solid fa-trash" /></a>
+                </div>
+            </div>
         </div>
         </>
     )
